50b9f646c3520968078a283367ede9dffb37528c
Roger-Melo <rogerw592@gmail.com>
2023-06-08 15:27:23 -0300

    estado inicial bateria 01

---
+++ bateria-de-exercicios-01/app.js
@@ -0,0 +1,138 @@
+/*
+Durante a resolução desta bateria de exercícios você verá erros 
+no console do navegador. 
+
+Fazer os erros desaparecerem é parte da resolução dos exercícios. 
+*/
+
+/*
+01
+
+Vincule este arquivo (app.js) ao index.html desta mesma pasta.
+*/
+
+/*
+02
+
+Conserte o nome da let abaixo.
+*/
+
+let numero impar = 7
+
+/* 
+03
+
+Faça a let acima receber o número 3. 
+
+Não modifique a declaração da let. 
+
+O código que você escrever deve estar abaixo deste comentário.
+*/
+
+
+
+/*
+04
+
+Faça a const abaixo ser exibida no console do navegador.
+
+Após ver o valor da const no console, remova do código o comando 
+que exibiu a const no console.
+*/
+
+const meExiba = 123
+
+/*
+05
+
+A const abaixo foi nomeada como uma abreviação.
+
+Apenas lendo a declaração dela, você sabe o que a abreviação "np" 
+significa?
+
+Provavelmente, não. Certo?
+
+O objetivo deste exercício é exatamente este. Deixar claro que 
+abreviações podem prejudicar o entendimento de um código. 
+
+O nome correto da const (sem abreviação) será revelado na próxima 
+aula. 
+*/
+
+const np = 2
+
+/* 
+06
+
+O código abaixo está lançando um erro no console. 
+
+O que você pode ajustar na declaração de "videoGame" para que o 
+erro não seja mais exibido?
+
+Leia a mensagem do erro no console. Ela já está te dando a dica 
+do que deve ser ajustado.
+
+Dica: o ajuste deve ser feito apenas na linha da declaração de 
+"videoGame". 
+*/
+
+const videoGame = 'Xbox'
+
+videoGame = 'Playstation'
+
+/*
+07
+
+Apenas lendo o código abaixo, você consegue saber qual valor a 
+const "final" armazenou?
+
+Após calcular o resultado em sua mente, exiba a const "final" no 
+console para conferir se o valor que você imaginou está certo. 
+*/
+
+const x = 0
+let y = 3
+let w = x
+
+w = 15 - y
+y = 5
+
+const final = x + y + 7 + w
+
+/*
+08
+
+Conserte a declaração da variável abaixo. 
+*/
+
+umNumeroQualquer = 9
+
+/*
+09
+
+Abaixo da declaração da const "curso", declare uma nova constante 
+chamada "sigla".
+
+O valor desta nova constante deve ser "CJRM". 
+
+Não insira manualmente nenhum dos caracteres da sigla "CJRM".
+
+Isto é, você não deve inserir diretamente o caractere "C", por 
+exemplo.
+
+O valor da constante "sigla" deve ser gerado através do valor 
+que a const "curso" armazena.
+
+Após gerar o valor da constante "sigla", exiba-a no console para 
+checar se você realmente gerou o valor certo ("CJRM"). 
+
+Após ver o valor no console, remova o console.log() do seu código.
+
+Leia a dica abaixo apenas se não conseguir fazer o exercício com 
+as informações acima. 
+
+Dica: use concatenação e sintaxe de acesso de caractere de string 
+para gerar o valor da constante "sigla".
+*/
+
+const curso = 'Curso de JavaScript Roger Melo'
